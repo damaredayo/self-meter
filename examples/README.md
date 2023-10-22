1bedc767de8abe24dc4cdce34d082a5ac6bc5cbd
Tyler Thompson <yoitscore@gmail.com>
2023-10-22 22:51:53 +0100

    Add testable examples

---
+++ examples/README.md
@@ -0,0 +1,13 @@
+# Self-Meter Examples
+
+## Usage
+
+You can run the examples by executing the following command:
+
+```bash
+# for json.rs
+cargo run --bin self-meter-json
+
+# for simple.rs
+cargo run --bin self-meter-simple
+```
--- examples/json.rs
+++ examples/json.rs
@@ -1,24 +1,30 @@
 extern crate self_meter;
 extern crate serde_json;
 
-use std::io::{Write, stderr};
-use std::time::Duration;
-use std::thread::sleep;
 use std::collections::BTreeMap;
-
+use std::io::{stderr, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let mut meter = self_meter::Meter::new(Duration::new(1, 0)).unwrap();
     meter.track_current_thread("main");
     loop {
-        meter.scan()
-            .map_err(|e| writeln!(&mut stderr(), "Scan error: {}", e)).ok();
-        println!("Report: {}",
-            serde_json::ser::to_string_pretty(&meter.report()).unwrap());
-        println!("Threads: {}",
+        meter
+            .scan()
+            .map_err(|e| writeln!(&mut stderr(), "Scan error: {}", e))
+            .ok();
+        println!(
+            "Report: {}",
+            serde_json::ser::to_string_pretty(&meter.report()).unwrap()
+        );
+        println!(
+            "Threads: {}",
             serde_json::ser::to_string_pretty(
-                &meter.thread_report().map(|x| x.collect::<BTreeMap<_,_>>())
-            ).unwrap());
+                &meter.thread_report().map(|x| x.collect::<BTreeMap<_, _>>())
+            )
+            .unwrap()
+        );
         let mut x = 0;
         for _ in 0..10000000 {
             x = u64::wrapping_mul(x, 7);
